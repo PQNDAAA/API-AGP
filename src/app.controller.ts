105b58dc41dfa25009f02465b752acaab7f7bb39
PQNDAAA <gayvallet.dylan@gmail.com>
2026-09-06 14:01:16 +0000

    SQL test, bdd OK

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('ic')
+  getInternalControls(){
+    return this.appService.getInternalControls();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import {DbService} from "./db/db.service";
 
 @Injectable()
 export class AppService {
+  constructor(private db: DbService){}
+
   getHello() {
     return {message: 'API AGP'};
+  }
+
+  async getInternalControls(){
+    const result = await this.db.query(
+        'SELECT * FROM internalcontrols'
+    );
+    return result.rows;
   }
 }
